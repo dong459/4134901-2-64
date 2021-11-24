3ac2c2eaf4080c603b256f0108631477bcdd01c1
dong459 <you@example.com>
2021-11-24 13:20:10 +0700

    first commit

---
+++ LAB3/hello-react/src/components/Name.js
@@ -0,0 +1,10 @@
+function MyName() {
+    return (
+        <div className="Myname">
+            <div>ขวัญชัย วุฒิวงศ์</div>      
+            <div>610112418004</div>
+        </div>
+    );
+}
+
+export default  MyName;
